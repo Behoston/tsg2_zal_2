a6fe93a70226be150fce86859ff861e29b378e5b
Kasia <katarzynabednarska94@gmail.com>
2018-01-14 19:59:30 +0100

    dodany parser inputu

--- io_utils.py
+++ io_utils.py
@@ -1,9 +1,14 @@
 def parse_input(input_file_name):
+    dane = []
     with open(input_file_name) as f:
         for line in f:
-            pass
+            line=line.strip()
+            if line[0]!='>':
+                dane.append(line)
+    return dane
 
 
 def dump_output(output_file_name, data):
     with open(output_file_name, 'w') as f:
         f.write(data)
+        
