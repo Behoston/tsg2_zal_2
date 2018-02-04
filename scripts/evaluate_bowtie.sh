d0728568263e5e4d075c04b422a3ee79bb3436c2
Behoston <mateusz@bothouse.co>
2018-02-04 14:53:11 +0100

    Move reference sequence.

--- scripts/evaluate_bowtie.sh
+++ scripts/evaluate_bowtie.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 cd app
-for FILE in 1 1_to_3 3_to_5
-do
- echo ${FILE}
- bowtie2 -a --local --mp 2,2 --rdg 10,2 --rfg 10,2 -f -x ./sample_data/reference/reference -U ./sample_data/reads_${FILE}_percent_bad.fasta | python ./evaluate.py
- echo
- echo
-done
+bowtie2 -a --local --mp 2,2 --rdg 10,2 --rfg 10,2 -f -x ./sample_data/reference/reference -U $1 | python ./evaluate.py
