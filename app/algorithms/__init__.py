fc97322ef8504534ee2f9231df71984ed8b7f69d
Kasia <katarzynabednarska94@gmail.com>
2018-02-07 19:21:24 +0100

    Dodany greedy scs.

--- app/algorithms/__init__.py
+++ app/algorithms/__init__.py
@@ -1,10 +1,12 @@
 from algorithms.olc import olc_suffix, olc_dynamic, olc
-from algorithms.scs import scs
+from algorithms.scs import scs as scs
+from algorithms.scs import scs as greedy_scs
 from algorithms.de_bruijn import do_assembly as de_bruijn
 
 
 algorithms = {
     'SCS': scs,
+    'GREEDY_SCS': greedy_scs,
     'DE_BRUIJN': de_bruijn,
     'OLC': olc,
     'OLC_SUFFIX': olc_suffix,
