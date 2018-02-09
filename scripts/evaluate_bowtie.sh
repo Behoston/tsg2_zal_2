3766e35d2a2714bb80796fa1b08f34646f9b1a2a
Behoston <mlegiecki@gmail.com>
2018-02-09 23:17:39 +0100

    Run evaluate from root dir.

--- scripts/evaluate_bowtie.sh
+++ scripts/evaluate_bowtie.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-cd app
-bowtie2 -a --local --mp 2,2 --rdg 10,2 --rfg 10,2 -f -x ./sample_data/reference/reference -U $1 | python ./evaluate.py
+bowtie2 -a --local --mp 2,2 --rdg 10,2 --rfg 10,2 -f -x ./app/sample_data/reference/reference -U $1 | python ./app/evaluate.py
