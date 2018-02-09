765d8bd7bc5956a93195fe6df35446c74f889ecd
Behoston <mlegiecki@gmail.com>
2018-02-09 22:58:40 +0100

    Default error correction.

--- app/assembly.py
+++ app/assembly.py
@@ -13,9 +13,9 @@ DEFAULT_ALGORITHM = 'OLC'
 @click.argument('output_file_name', required=False, default='output.fasta')
 @click.option('--algorithm', required=False, default=DEFAULT_ALGORITHM,
               type=click.Choice([key for key in algorithms.keys()]))
-@click.option('--error_correction', is_flag=True)
-def _assembly(input_file_name, output_file_name, algorithm, error_correction):
-    return assembly(input_file_name, output_file_name, algorithm, error_correction)
+@click.option('--no-error_correction', is_flag=True)
+def _assembly(input_file_name, output_file_name, algorithm, no_error_correction):
+    return assembly(input_file_name, output_file_name, algorithm, error_correction=not no_error_correction)
 
 
 def assembly(input_file_name, output_file_name, algorithm, error_correction):
