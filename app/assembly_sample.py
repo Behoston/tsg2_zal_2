1d204fa78ba21b9b398ebfa64b9f3e66f9df8030
Behoston <mlegiecki@gmail.com>
2018-02-09 23:15:58 +0100

    Fix sample assembly.

--- app/assembly_sample.py
+++ app/assembly_sample.py
@@ -7,9 +7,10 @@ SAMPLE_FILES = [
 
 if __name__ == '__main__':
     for sample_file in SAMPLE_FILES:
+        output_file_name = sample_file.split('.')[-2].split('/')[-1]
         assembly(
             input_file_name=sample_file,
-            output_file_name=f'./sample_data/output_{sample_file}_percent_bad.fasta',
+            output_file_name=f'./sample_data/output_{output_file_name}.fasta',
             algorithm=DEFAULT_ALGORITHM,
             error_correction=True,
         )
