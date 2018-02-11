a9048ba8835bc549ac2ff1314c62311490bbd622
Behoston <mlegiecki@gmail.com>
2018-02-11 18:55:18 +0100

    OLC - set as default instead of naive OLC.

--- app/assembly.py
+++ app/assembly.py
@@ -5,7 +5,7 @@ from algorithms.error_corrections import CorrectedReads
 from io_utils import dump_output
 from io_utils import parse_input
 
-DEFAULT_ALGORITHM = 'OLC_NAIVE'
+DEFAULT_ALGORITHM = 'OLC'
 
 
 @click.command()
